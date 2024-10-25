d811ace6653a388f3173a8192a2e7a2a25b692f2
Nasiful Alam <nasifulalam1212@gmail.com>
2024-10-26 03:11:01 +0600

    Updated

--- HSC_Rankings/settings.py
+++ HSC_Rankings/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,6 +117,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
